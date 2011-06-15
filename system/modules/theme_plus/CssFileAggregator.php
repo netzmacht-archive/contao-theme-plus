acd26650cb9c96d4e87a1aef67fa7ec892528f1b
Tristan Lins <tristan.lins@infinitysoft.de>
2011-06-16 01:15:00 +0200

    Add aggregator classes.

---
+++ system/modules/theme_plus/CssFileAggregator.php
@@ -0,0 +1,173 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+#copyright
+
+
+/**
+ * Class CssFileAggregator
+ * 
+ * 
+ * @copyright  InfinitySoft 2011
+ * @author     Tristan Lins <tristan.lins@infinitysoft.de>
+ * @package    Layout Additional Sources
+ */
+class CssFileAggregator extends ThemePlusFile {
+	
+	/**
+	 * The files to aggregate.
+	 */
+	protected $arrFiles;
+	
+	
+	/**
+	 * The absolutize page.
+	 */
+	protected $objAbsolutizePage;
+	
+	
+	/**
+	 * The aggregated file.
+	 */
+	protected $strAggregatedFile;
+	
+	
+	/**
+	 * Create a new css file object.
+	 */
+	public function __construct()
+	{
+		$args = func_get_args();
+		if (!($args[count($args)-1] instanceof ThemePlusFile))
+		{
+			$this->objAbsolutizePage = array_pop($args);
+		}
+		else
+		{
+			$this->objAbsolutizePage = false;
+		}
+		$this->arrFiles = $args;
+		$this->strAggregatedFile = null;
+	}
+	
+	
+	/**
+	 * Add a file.
+	 */
+	public function add(LocalCssFile $objFile)
+	{
+		$this->arrFiles[] = $objFile;
+	}
+
+	
+	public function getFile()
+	{
+		if ($this->strAggregatedFile == null)
+		{
+			$arrFiles = array();
+			$strKey = count($this->arrFiles);
+			foreach ($this->arrFiles as $objThemePlusFile)
+			{
+				if ($objThemePlusFile instanceof LocalCssFile)
+				{
+					if ($objThemePlusFile->isAggregateable())
+					{
+						$strFile = $objThemePlusFile->getFile();
+						$objFile = new File($strFile);
+						$arrFiles[] = $strFile;
+						$strKey .= sprintf(':%s-%d', basename($strFile, '.css'), $objFile->mtime);
+						continue; 
+					}
+				}
+				throw new Exception('Could not aggreagate the file: ' . $objFile);
+			}
+			
+			$strTemp = 'system/html/stylesheet-' . substr(md5($strKey), 0, 8) . '.css';
+			
+			if (!file_exists(TL_ROOT . '/' . $strTemp))
+			{
+				$this->import('Compression');
+				
+				// import the css minimizer
+				$strCssMinimizerClass = $this->Compression->getDefaultCssMinimizerClass();
+				$this->import($strCssMinimizerClass, 'Minimizer');
+				
+				// import the gzip compressor
+				$strGzipCompressorClass = $this->Compression->getCompressorClass('gzip');
+				$this->import($strGzipCompressorClass, 'Compressor');
+				
+				// import the url remapper
+				$this->import('CssUrlRemapper');
+				
+				// build the content
+				$strContent = '@charset "UTF-8";' . "\n";
+				
+				foreach ($arrFiles as $strFile)
+				{
+					$objFile = new File($strFile);
+					
+					// get the css code
+					$strSubContent = $objFile->getContent();
+					
+					// detect and decompress gziped content
+					$strSubContent = ThemePlus::decompressGzip($strSubContent);
+					
+					// handle @charset
+					$strSubContent = $this->handleCharset($strSubContent);
+									
+					// remap url(..) entries
+					$strSubContent = $this->CssUrlRemapper->remapCode($strSubContent, $strFile, $strTemp, $this->objAbsolutizePage ? true : false, $this->objAbsolutizePage);
+					
+					// append to content
+					$strContent .= "\n" . $strSubContent;
+				}
+				
+				// minify
+				if (!$this->Minimizer->minimizeToFile($strTemp, $strContent))
+				{
+					// write unminified code, if minify failed
+					$objTemp = new File($strTemp);
+					$objTemp->write($strContent);
+					$objTemp->close();
+				}
+				
+				// create the gzip compressed version
+				if (!$GLOBALS['TL_CONFIG']['theme_plus_gz_compression_disabled'])
+				{
+					$this->Compressor->compress($strTemp, $strTemp . '.gz');
+				}
+			}
+			
+			$this->strAggregatedFile = $strTemp;
+		}
+		return $this->strAggregatedFile;
+	}
+	
+	
+	public function getEmbededHtml()
+	{
+		// get the file
+		$strFile = $this->getFile();
+		$objFile = new File($strFile);
+		
+		// get the css code
+		$strContent = $objFile->getContent();
+		
+		// handle @charset
+		$strContent = $this->handleCharset($strContent);
+		
+		// return html code
+		return '<style type="text/css">' . $strContent . '</style>';
+	}
+	
+	
+	public function getIncludeHtml()
+	{
+		// get the file
+		$strFile = $this->getFile();
+		
+		// return html code
+		return '<link type="text/css" rel="stylesheet" href="' . specialchars($strFile) . '" />';
+	}
+}
+
+?>
---
+++ system/modules/theme_plus/JavaScriptFileAggregator.php
@@ -0,0 +1,152 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+#copyright
+
+
+/**
+ * Class JavaScriptFileAggregator
+ * 
+ * 
+ * @copyright  InfinitySoft 2011
+ * @author     Tristan Lins <tristan.lins@infinitysoft.de>
+ * @package    Layout Additional Sources
+ */
+class JavaScriptFileAggregator extends ThemePlusFile {
+	
+	/**
+	 * The files to aggregate.
+	 */
+	protected $arrFiles;
+	
+	
+	/**
+	 * The aggregated file.
+	 */
+	protected $strAggregatedFile;
+	
+	
+	/**
+	 * Create a new css file object.
+	 */
+	public function __construct()
+	{
+		$this->arrFiles = func_get_args();
+		$this->strAggregatedFile = null;
+	}
+	
+	
+	/**
+	 * Add a file.
+	 */
+	public function add(LocalJavaScriptFile $objFile)
+	{
+		$this->arrFiles[] = $objFile;
+	}
+
+	
+	public function getFile()
+	{
+		if ($this->strAggregatedFile == null)
+		{
+			$arrFiles = array();
+			$strKey = count($this->arrFiles);
+			foreach ($this->arrFiles as $objThemePlusFile)
+			{
+				if ($objThemePlusFile instanceof LocalJavaScriptFile)
+				{
+					if ($objThemePlusFile->isAggregateable())
+					{
+						$strFile = $objThemePlusFile->getFile();
+						$objFile = new File($strFile);
+						$arrFiles[] = $strFile;
+						$strKey .= sprintf(':%s-%d', basename($strFile, '.css'), $objFile->mtime);
+						continue;
+					}
+				}
+				throw new Exception('Could not aggreagate the file: ' . $objFile);
+			}
+			
+			$strTemp = 'system/html/javascript-' . substr(md5($strKey), 0, 8) . '.css';
+			
+			if (!file_exists(TL_ROOT . '/' . $strTemp))
+			{
+				$this->import('Compression');
+				
+				// import the javascript minimizer
+				$strJsMinimizerClass = $this->Compression->getDefaultJsMinimizerClass();
+				$this->import($strJsMinimizerClass, 'Minimizer');
+				
+				// import the gzip compressor
+				$strGzipCompressorClass = $this->Compression->getCompressorClass('gzip');
+				$this->import($strGzipCompressorClass, 'Compressor');
+				
+				// build the content
+				$strContent = '';
+				
+				foreach ($arrFiles as $strFile)
+				{
+					$objFile = new File($strFile);
+					
+					// get the css code
+					$strSubContent = $objFile->getContent();
+					
+					// detect and decompress gziped content
+					$strSubContent = ThemePlus::decompressGzip($strSubContent);
+					
+					// append to content
+					$strContent .= "\n" . $strSubContent;
+				}
+				
+				// minify
+				if (!$this->Minimizer->minimizeToFile($strTemp, $strContent))
+				{
+					// write unminified code, if minify failed
+					$objTemp = new File($strTemp);
+					$objTemp->write($strContent);
+					$objTemp->close();
+				}
+				
+				// create the gzip compressed version
+				if (!$GLOBALS['TL_CONFIG']['theme_plus_gz_compression_disabled'])
+				{
+					$this->Compressor->compress($strTemp, $strTemp . '.gz');
+				}
+			}
+			
+			$this->strAggregatedFile = $strTemp;
+		}
+		return $this->strAggregatedFile;
+	}
+	
+	
+	/**
+	 * Get embeded html code
+	 */
+	public function getEmbededHtml()
+	{
+		// get the file
+		$strFile = $this->getFile();
+		$objFile = new File($strFile);
+		
+		// get the css code
+		$strContent = $objFile->getContent();
+		
+		// return html code
+		return '<script type="text/javascript">' . $strContent . '</script>';
+	}
+	
+	
+	/**
+	 * Get included html code
+	 */
+	public function getIncludeHtml()
+	{
+		// get the file
+		$strFile = $this->getFile();
+		
+		// return html code
+		return '<script type="text/javascript" src="' . specialchars($strFile) . '"></script>';
+	}
+}
+
+?>
