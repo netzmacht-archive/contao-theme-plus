3b20d649dc7c955e13d406e905031eb513d6fad6
Tristan Lins <tristan.lins@infinitysoft.de>
2011-06-22 21:12:14 +0200

    Add auto upgrade from layout_additional_sources to theme_plus.

--- system/modules/theme_plus/runonce/runonce.php
+++ system/modules/theme_plus/runonce/runonce.php
@@ -48,7 +48,7 @@ class ThemePlusRunonce extends Frontend
 	 */
 	protected function testYUI()
 	{
-		$strCmd = escapeshellcmd('yui-compressor');
+		$strCmd = escapeshellcmd($GLOBALS['TL_CONFIG']['additional_sources_yui_cmd']);
 		$proc = proc_open(
 			$strCmd,
 			array(
@@ -78,9 +78,10 @@ class ThemePlusRunonce extends Frontend
 
 	public function run()
 	{
+		$this->loadLanguageFile('theme_plus');
 		$this->upgrade1_5();
 		$this->upgrade1_6();
-		$this->upgrade1_8();
+		$this->upgrade2_0();
 		$this->checkCompression();
 	}
 	
@@ -90,23 +91,23 @@ class ThemePlusRunonce extends Frontend
 	 */
 	protected function upgrade1_5()
 	{
-		if ($this->Database->tableExists('tl_theme_plus_file') && !$this->Database->fieldExists('additional_source', 'tl_layout'))
+		if ($this->Database->tableExists('tl_additional_source', false, true) && !$this->Database->fieldExists('additional_source', 'tl_layout', true))
 		{
-			$this->Database->execute('ALTER TABLE tl_layout ADD additional_source blob NULL');
+			$this->Database->executeUncached('ALTER TABLE tl_layout ADD additional_source blob NULL');
 			
 			// go over all themes
-			$objTheme = $this->Database->execute("SELECT * FROM tl_theme");
+			$objTheme = $this->Database->executeUncached("SELECT * FROM tl_theme");
 			while ($objTheme->next())
 			{
 				// list all additional sources
 				$objAdditionalSource = $this->Database->prepare("
-						SELECT * FROM tl_theme_plus_file WHERE pid=?")
-					->execute($objTheme->id);
+						SELECT * FROM tl_additional_source WHERE pid=?")
+					->executeUncached($objTheme->id);
 				
 				// go over all theme layouts
 				$objLayout = $this->Database->prepare("
 						SELECT * FROM tl_layout WHERE pid=?")
-					->execute($objTheme->id);
+					->executeUncached($objTheme->id);
 				while ($objLayout->next())
 				{
 					$arrAdditionalSource = array();
@@ -132,12 +133,12 @@ class ThemePlusRunonce extends Frontend
 					
 					$this->Database->prepare("
 							UPDATE tl_layout SET additional_source=? WHERE id=?")
-						->execute(serialize($arrAdditionalSource), $objLayout->id);
+						->executeUncached(serialize($arrAdditionalSource), $objLayout->id);
 				}
 			}
 			
-			$this->loadLanguageFile('theme_plus');
-			$_SESSION['TL_CONFIRM'][] = $GLOBALS['TL_LANG']['theme_plus']['upgrade1.5'];
+			# add backend message
+			$GLOBALS['TL_HOOKS']['parseBackendTemplate'][] = array('ThemePlusRunonce', 'addUpgrade1dot5Message');
 		}
 	}
 	
@@ -173,11 +174,277 @@ class ThemePlusRunonce extends Frontend
 	
 	
 	/**
-	 * Configuration upgrade to 2.0
+	 * Configuration and database upgrade to 2.0
 	 */
 	protected function upgrade2_0()
 	{
-		
+		# upgrade from layout_additional_sources
+		if ($this->Database->tableExists('tl_additional_source', false, true))
+		{
+			# update configuration from layout_additional_sources to theme+
+			
+			# update css compression mode
+			if (!empty($GLOBALS['TL_CONFIG']['additional_sources_css_compression']))
+			{
+				# less mode
+				if (preg_match('#^less.js\+(.*)$#', $GLOBALS['TL_CONFIG']['additional_sources_css_compression'], $m))
+				{
+					// less is in precompiled mode
+					$this->updateConfig('theme_plus_lesscss_mode', 'less.js+pre');
+					
+					if ($m[1] != 'pre')
+					{
+						if (!isset($GLOBALS['TL_CONFIG']['default_css_minimizer']) || $GLOBALS['TL_CONFIG']['default_css_minimizer'] == 'none')
+						{
+							$this->updateConfig('default_css_minimizer', $m[1]);
+						}
+					}
+				}
+				
+				# no less mode
+				else if ($GLOBALS['TL_CONFIG']['additional_sources_css_compression'] != 'less.js')
+				{
+					if (empty($GLOBALS['TL_CONFIG']['default_css_minimizer']) || $GLOBALS['TL_CONFIG']['default_css_minimizer'] == 'none')
+					{
+						$this->updateConfig('default_css_minimizer', $GLOBALS['TL_CONFIG']['additional_sources_css_compression']);
+					}
+				}
+				
+			}
+			
+			# update javascript compression mode
+			if (!empty($GLOBALS['TL_CONFIG']['additional_sources_js_compression']))
+			{
+				if (empty($GLOBALS['TL_CONFIG']['default_js_minimizer']) || $GLOBALS['TL_CONFIG']['default_js_minimizer'] == 'none')
+				{
+					$this->updateConfig('default_js_minimizer', $GLOBALS['TL_CONFIG']['additional_sources_js_compression']);
+				}
+				
+			}
+			
+			# update gzip disable mode
+			if (isset($GLOBALS['TL_CONFIG']['additional_sources_gz_compression_disabled']) && $GLOBALS['TL_CONFIG']['additional_sources_gz_compression_disabled'])
+			{
+				$this->updateConfig('theme_plus_gz_compression_disabled', 1);
+			}
+			
+			# delete outdated settings
+			$this->deleteConfig('additional_sources_combination');
+			$this->deleteConfig('additional_sources_css_compression');
+			$this->deleteConfig('additional_sources_js_compression');
+			$this->deleteConfig('additional_sources_gz_compression_disabled');
+			$this->deleteConfig('additional_sources_hide_cssmin_message');
+			$this->deleteConfig('additional_sources_hide_jsmin_message');
+			
+			# update database
+			
+			# create table tl_theme_plus_file
+			$blnTableThemePlusFileExists = false; // flag required because tableExists caching is faulty, do not remove!
+			if (!$this->Database->tableExists('tl_theme_plus_file', false, true))
+			{
+				try {
+					$this->Database->executeUncached("CREATE TABLE `tl_theme_plus_file` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `pid` int(10) unsigned NOT NULL default '0',
+  `sorting` int(10) unsigned NOT NULL default '0',
+  `tstamp` int(10) unsigned NOT NULL default '0',
+  `type` varchar(32) NOT NULL default '',
+  `js_file` blob NULL,
+  `js_url` blob NULL,
+  `css_file` blob NULL,
+  `css_url` blob NULL,
+  `media` blob NULL,
+  `editor_integration` blob NULL,
+  `force_editor_integration` char(1) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  KEY `pid` (`pid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+					$blnTableThemePlusFileExists = true;
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			else
+			{
+				$blnTableThemePlusFileExists = true;
+			}
+			
+			# convert from tl_additional_source to tl_theme_plus_file
+			if ($blnTableThemePlusFileExists && $this->Database->tableExists('tl_additional_source', false, true))
+			{
+				try {
+					$this->Database->executeUncached("INSERT INTO tl_theme_plus_file (id,pid,sorting,tstamp,type,js_file,js_url,css_file,css_url,media,editor_integration,force_editor_integration)
+							SELECT id,pid,sorting,tstamp,type,js_file,js_url,css_file,css_url,media,editor_integration,force_editor_integration FROM tl_additional_source");
+					$objFile = $this->Database->executeUncached("SELECT * FROM tl_theme_plus_file");
+					while ($objFile->next())
+					{
+						$arrMedia = deserialize($objFile->media, true);
+						if (count($arrMedia))
+						{
+							$this->Database->prepare("UPDATE tl_theme_plus_file SET media=? WHERE id=?")
+								->executeUncached(implode(',', $arrMedia), $objFile->id);
+						}
+					}
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			
+			# drop tl_additional_source
+			if ($this->Database->tableExists('tl_additional_source', false, true))
+			{
+				try {
+					$this->Database->executeUncached("DROP TABLE tl_additional_source");
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			
+			# convert tl_layout.additional_source to tl_layout.theme_plus_files
+			if ($this->Database->fieldExists('additional_source', 'tl_layout', true) && !$this->Database->fieldExists('theme_plus_files', 'tl_layout', true))
+			{
+				try {
+					$this->Database->executeUncached("ALTER TABLE tl_layout CHANGE additional_source theme_plus_files blob NULL");
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			
+			# convert tl_page.additional_source to tl_page.theme_plus_files
+			$blnFieldThemePlusFilesTLPageExists = $this->Database->fieldExists('theme_plus_files', 'tl_page', true);
+			if ($this->Database->fieldExists('additional_source', 'tl_page', true) && !$blnFieldThemePlusFilesTLPageExists)
+			{
+				try {
+					$this->Database->executeUncached("ALTER TABLE tl_page CHANGE additional_source theme_plus_files blob NULL");
+					$blnFieldThemePlusFilesTLPageExists = true;
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			
+			# add new include flag fields
+			$blnFieldThemePlusIncludeFilesTLPageExists = $this->Database->fieldExists('theme_plus_include_files', 'tl_page', true);
+			if (!$blnFieldThemePlusIncludeFilesTLPageExists)
+			{
+				try {
+					$this->Database->executeUncached("ALTER TABLE tl_page ADD theme_plus_include_files char(1) NOT NULL default ''");
+					$blnFieldThemePlusIncludeFilesTLPageExists = true;
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			
+			# set the include flag field
+			if ($blnFieldThemePlusIncludeFilesTLPageExists && $blnFieldThemePlusFilesTLPageExists)
+			{
+				try {
+					$objPage = $this->Database->executeUncached("SELECT * FROM tl_page");
+					$arrIds = array();
+					while ($objPage->next())
+					{
+						$arrFiles = deserialize($objPage->theme_plus_files, true);
+						if (count($arrFiles))
+						{
+							$arrIds[] = $objPage->id;
+						}
+					}
+					if (count($arrIds))
+					{
+						$this->Database->executeUncached("UPDATE tl_page SET theme_plus_include_files='1' WHERE id IN (" . implode(',', $arrIds) . ")");
+					}
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			
+			# remove layout_additional_sources
+			try {
+				# check repository installation
+				$objInstall = $this->Database->executeUncached("SELECT * FROM tl_repository_installs WHERE extension='layout_additional_sources'");
+				if ($objInstall->next())
+				{
+					$this->Database->prepare("DELETE FROM tl_repository_instfiles WHERE pid=?")
+						->executeUncached($objInstall->id);
+					$this->Database->prepare("DELETE FROM tl_repository_installs WHERE id=?")
+						->executeUncached($objInstall->id);
+				}
+			} catch(Exception $e) {
+				try {
+					$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+				} catch(Exception $e) {}
+			}
+			
+			if (is_dir(TL_ROOT . '/system/modules/layout_additional_sources'))
+			{
+				try {
+					$this->import('Files');
+					# remove module directory
+					$this->Files->rrdir('system/modules/layout_additional_sources');
+				} catch(Exception $e) {
+					try {
+						$this->log($e->getMessage(), 'ThemePlusRunonce::upgrade2_0', TL_ERROR);
+					} catch(Exception $e) {}
+				}
+			}
+			
+			# remove callbacks ...
+			# ... from generatePage hook
+			if (is_array($GLOBALS['TL_HOOKS']['generatePage']))
+			{
+				foreach ($GLOBALS['TL_HOOKS']['generatePage'] as $k=>$v)
+				{
+					if ($v[0] == 'LayoutAdditionalSources')
+					{
+						unset($GLOBALS['TL_HOOKS']['generatePage'][$k]);
+					}
+				}
+			}
+			# ... from replaceInsertTags hook
+			if (is_array($GLOBALS['TL_HOOKS']['replaceInsertTags']))
+			{
+				foreach ($GLOBALS['TL_HOOKS']['replaceInsertTags'] as $k=>$v)
+				{
+					if ($v[0] == 'LayoutAdditionalSources')
+					{
+						unset($GLOBALS['TL_HOOKS']['replaceInsertTags'][$k]);
+					}
+				}
+			}
+			# ... from parseBackendTemplate hook
+			if (is_array($GLOBALS['TL_HOOKS']['parseBackendTemplate']))
+			{
+				foreach ($GLOBALS['TL_HOOKS']['parseBackendTemplate'] as $k=>$v)
+				{
+					if ($v[0] == 'LayoutAdditionalSourcesBackend')
+					{
+						unset($GLOBALS['TL_HOOKS']['parseBackendTemplate'][$k]);
+					}
+				}
+			}
+			
+			# remove easy_themes integration
+			if (isset($GLOBALS['TL_EASY_THEMES_MODULES']['additional_source']))
+			{
+				unset($GLOBALS['TL_EASY_THEMES_MODULES']['additional_source']);
+			}
+			
+			# add backend message
+			$GLOBALS['TL_HOOKS']['parseBackendTemplate'][] = array('ThemePlusRunonce', 'addUpgrade2dot0Message');
+		}
 	}
 	
 	
@@ -238,6 +505,38 @@ class ThemePlusRunonce extends Frontend
 				$this->updateConfig('additional_sources_js_compression', 'none');
 			}
 		}
+	}
+
+
+	/**
+	 * Add message to backend.
+	 */
+	public function addUpgrade1dot5Message($strContent, $strTemplate)
+	{
+		if ($strTemplate == 'be_main')
+		{
+			$strContent = str_replace(
+				'<div id="container">',
+				sprintf('<p style="border: 1px solid #BBBBBB; margin: 18px auto; text-align: left; width: 914px;" class="tl_info">%s</p><div id="container">', $GLOBALS['TL_LANG']['theme_plus']['upgrade1.5']),
+				$strContent);
+		}
+		return $strContent;
+	}
+
+
+	/**
+	 * Add message to backend.
+	 */
+	public function addUpgrade2dot0Message($strContent, $strTemplate)
+	{
+		if ($strTemplate == 'be_main')
+		{
+			$strContent = str_replace(
+				'<div id="container">',
+				sprintf('<p style="border: 1px solid #BBBBBB; margin: 18px auto; text-align: left; width: 914px;" class="tl_info">%s</p><div id="container">', $GLOBALS['TL_LANG']['theme_plus']['upgrade2.0']),
+				$strContent);
+		}
+		return $strContent;
 	}
 }
 
