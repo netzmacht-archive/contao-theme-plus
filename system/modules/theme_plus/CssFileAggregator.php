da8a58ff7f39a01ca32d5e96ab70b3fd4dbe6473
Tristan Lins <tristan.lins@infinitysoft.de>
2011-08-30 13:41:20 +0200

    Improve aggregator, trim the single file content and only add non-empty contents.

--- system/modules/theme_plus/CssFileAggregator.php
+++ system/modules/theme_plus/CssFileAggregator.php
@@ -7,25 +7,25 @@
  * Class CssFileAggregator
  */
 class CssFileAggregator extends LocalThemePlusFile {
-	
+
 	/**
 	 * The files to aggregate.
 	 */
 	protected $arrFiles;
-	
-	
+
+
 	/**
 	 * The absolutize page.
 	 */
 	protected $objAbsolutizePage;
-	
-	
+
+
 	/**
 	 * The aggregated file.
 	 */
 	protected $strAggregatedFile;
-	
-	
+
+
 	/**
 	 * Create a new css file object.
 	 */
@@ -42,12 +42,12 @@ class CssFileAggregator extends LocalThemePlusFile {
 		}
 		$this->arrFiles = $args;
 		$this->strAggregatedFile = null;
-		
+
 		// import the Theme+ master class
 		$this->import('ThemePlus');
 	}
-	
-	
+
+
 	/**
 	 * Add a file.
 	 */
@@ -56,7 +56,7 @@ class CssFileAggregator extends LocalThemePlusFile {
 		$this->arrFiles[] = $objFile;
 	}
 
-	
+
 	public function getFile()
 	{
 		if ($this->strAggregatedFile == null)
@@ -73,88 +73,94 @@ class CssFileAggregator extends LocalThemePlusFile {
 						$objFile = new File($strFile);
 						$arrFiles[] = $strFile;
 						$strKey .= sprintf(':%s-%d', basename($strFile, '.css'), $objFile->mtime);
-						continue; 
+						continue;
 					}
 				}
 				throw new Exception('Could not aggreagate the file: ' . $objFile);
 			}
-			
+
 			$strTemp = 'system/scripts/stylesheet-' . substr(md5($strKey), 0, 8) . '.css';
-			
+
 			if (!file_exists(TL_ROOT . '/' . $strTemp))
 			{
 				$this->import('Compression');
-				
+
 				// import the gzip compressor
 				$strGzipCompressorClass = $this->Compression->getCompressorClass('gzip');
 				$this->import($strGzipCompressorClass, 'Compressor');
-				
+
 				// import the url remapper
 				$this->import('CssUrlRemapper');
-				
+
 				// build the content
 				$strContent = '@charset "UTF-8";' . "\n";
-				
+
 				foreach ($arrFiles as $strFile)
 				{
 					$objFile = new File($strFile);
-					
+
 					// get the css code
 					$strSubContent = $objFile->getContent();
-					
+
 					// detect and decompress gziped content
 					$strSubContent = $this->ThemePlus->decompressGzip($strSubContent);
-					
+
 					// handle @charset
 					$strSubContent = $this->ThemePlus->handleCharset($strSubContent);
-									
+
 					// remap url(..) entries
 					$strSubContent = $this->CssUrlRemapper->remapCode($strSubContent, $strFile, $strTemp, $this->objAbsolutizePage ? true : false, $this->objAbsolutizePage);
-					
+
+					// trim content
+					$strSubContent = trim($strSubContent);
+
 					// append to content
-					$strContent .= "\n" . $strSubContent;
+					if (strlen($strSubContent)>0)
+					{
+						$strContent .= $strSubContent . "\n";
+					}
 				}
-				
+
 				// write the file
 				$objTemp = new File($strTemp);
 				$objTemp->write($strContent);
 				$objTemp->close();
-				
+
 				// create the gzip compressed version
 				if ($GLOBALS['TL_CONFIG']['gzipScripts'])
 				{
 					$this->Compressor->compress($strTemp, $strTemp . '.gz');
 				}
 			}
-			
+
 			$this->strAggregatedFile = $strTemp;
 		}
 		return $this->strAggregatedFile;
 	}
-	
-	
+
+
 	public function getEmbededHtml()
 	{
 		// get the file
 		$strFile = $this->getFile();
 		$objFile = new File($strFile);
-		
+
 		// get the css code
 		$strContent = $objFile->getContent();
-		
+
 		// handle @charset
 		$strContent = $this->ThemePlus->handleCharset($strContent);
-		
+
 		// return html code
 		return '<style type="text/css">' . $strContent . '</style>';
 	}
-	
-	
+
+
 	public function getIncludeHtml()
 	{
 		// get the file
 		$strFile = $this->getFile();
-		
+
 		// return html code
 		return '<link type="text/css" rel="stylesheet" href="' . TL_SCRIPT_URL . specialchars($strFile) . '" />';
 	}
--- system/modules/theme_plus/JavaScriptFileAggregator.php
+++ system/modules/theme_plus/JavaScriptFileAggregator.php
@@ -7,19 +7,19 @@
  * Class JavaScriptFileAggregator
  */
 class JavaScriptFileAggregator extends LocalThemePlusFile {
-	
+
 	/**
 	 * The files to aggregate.
 	 */
 	protected $arrFiles;
-	
-	
+
+
 	/**
 	 * The aggregated file.
 	 */
 	protected $strAggregatedFile;
-	
-	
+
+
 	/**
 	 * Create a new css file object.
 	 */
@@ -28,8 +28,8 @@ class JavaScriptFileAggregator extends LocalThemePlusFile {
 		$this->arrFiles = func_get_args();
 		$this->strAggregatedFile = null;
 	}
-	
-	
+
+
 	/**
 	 * Add a file.
 	 */
@@ -38,7 +38,7 @@ class JavaScriptFileAggregator extends LocalThemePlusFile {
 		$this->arrFiles[] = $objFile;
 	}
 
-	
+
 	public function getFile()
 	{
 		if ($this->strAggregatedFile == null)
@@ -60,55 +60,61 @@ class JavaScriptFileAggregator extends LocalThemePlusFile {
 				}
 				throw new Exception('Could not aggreagate the file: ' . $objFile);
 			}
-			
+
 			$strTemp = 'system/scripts/javascript-' . substr(md5($strKey), 0, 8) . '.js';
-			
+
 			if (!file_exists(TL_ROOT . '/' . $strTemp))
 			{
 				$this->import('Compression');
-				
+
 				// import the Theme+ master class
 				$this->import('ThemePlus');
-				
+
 				// import the gzip compressor
 				$strGzipCompressorClass = $this->Compression->getCompressorClass('gzip');
 				$this->import($strGzipCompressorClass, 'Compressor');
-				
+
 				// build the content
 				$strContent = '';
-				
+
 				foreach ($arrFiles as $strFile)
 				{
 					$objFile = new File($strFile);
-					
+
 					// get the css code
 					$strSubContent = $objFile->getContent();
-					
+
 					// detect and decompress gziped content
 					$strSubContent = $this->ThemePlus->decompressGzip($strSubContent);
-					
+
+					// trim content
+					$strSubContent = trim($strSubContent);
+
 					// append to content
-					$strContent .= "\n" . $strSubContent;
+					if (strlen($strSubContent)>0)
+					{
+						$strContent .= $strSubContent . "\n";
+					}
 				}
-				
+
 				// write the file
 				$objTemp = new File($strTemp);
 				$objTemp->write($strContent);
 				$objTemp->close();
-				
+
 				// create the gzip compressed version
 				if ($GLOBALS['TL_CONFIG']['gzipScripts'])
 				{
 					$this->Compressor->compress($strTemp, $strTemp . '.gz');
 				}
 			}
-			
+
 			$this->strAggregatedFile = $strTemp;
 		}
 		return $this->strAggregatedFile;
 	}
-	
-	
+
+
 	/**
 	 * Get embeded html code
 	 */
@@ -117,15 +123,15 @@ class JavaScriptFileAggregator extends LocalThemePlusFile {
 		// get the file
 		$strFile = $this->getFile();
 		$objFile = new File($strFile);
-		
+
 		// get the css code
 		$strContent = $objFile->getContent();
-		
+
 		// return html code
 		return '<script type="text/javascript">' . $strContent . '</script>';
 	}
-	
-	
+
+
 	/**
 	 * Get included html code
 	 */
@@ -133,7 +139,7 @@ class JavaScriptFileAggregator extends LocalThemePlusFile {
 	{
 		// get the file
 		$strFile = $this->getFile();
-		
+
 		// return html code
 		return '<script type="text/javascript" src="' . specialchars($strFile) . '"></script>';
 	}
