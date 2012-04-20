e09143ee445c763ee1b608455b8f3c7b70f939f1
Tristan Lins <tristan.lins@infinitysoft.de>
2012-04-20 11:22:43 +0200

    Add upgrade to 3.0 logic.

--- src/system/modules/theme_plus/runonce/runonce.php
+++ src/system/modules/theme_plus/runonce/runonce.php
@@ -79,12 +79,17 @@ class ThemePlusRunonce extends Frontend
 
 	public function run()
 	{
-		$this->loadLanguageFile('theme_plus');
-		$this->upgrade1_5();
-		$this->upgrade1_6();
-		$this->upgrade2_0();
-		$this->upgrade2_2();
-		$this->checkCompression();
+		try {
+			$this->loadLanguageFile('theme_plus');
+			$this->upgrade1_5();
+			$this->upgrade1_6();
+			$this->upgrade2_0();
+			$this->upgrade2_2();
+			$this->upgrade3_0();
+			$this->checkCompression();
+		} catch(Exception $e) {
+			$this->log($e->getMessage() . ";\n" . $e->getTraceAsString(), $e->getFile(), TL_ERROR);
+		}
 	}
 
 
@@ -93,7 +98,7 @@ class ThemePlusRunonce extends Frontend
 	 */
 	protected function upgrade1_5()
 	{
-		if ($this->Database->tableExists('tl_additional_source', false, true) && !$this->Database->fieldExists('additional_source', 'tl_layout', true)) {
+		if ($this->Database->tableExists('tl_additional_source', null, true) && !$this->Database->fieldExists('additional_source', 'tl_layout', true)) {
 			$this->Database->executeUncached('ALTER TABLE tl_layout ADD additional_source blob NULL');
 
 			// go over all themes
@@ -173,7 +178,7 @@ class ThemePlusRunonce extends Frontend
 	protected function upgrade2_0()
 	{
 		# upgrade from layout_additional_sources
-		if ($this->Database->tableExists('tl_additional_source', false, true)) {
+		if ($this->Database->tableExists('tl_additional_source', null, true)) {
 			# update configuration from layout_additional_sources to theme+
 
 			# update css compression mode
@@ -224,7 +229,7 @@ class ThemePlusRunonce extends Frontend
 
 			# create table tl_theme_plus_file
 			$blnTableThemePlusFileExists = false; // flag required because tableExists caching is faulty, do not remove!
-			if (!$this->Database->tableExists('tl_theme_plus_file', false, true)) {
+			if (!$this->Database->tableExists('tl_theme_plus_file', null, true)) {
 				try {
 					$this->Database->executeUncached("CREATE TABLE `tl_theme_plus_file` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -256,7 +261,7 @@ class ThemePlusRunonce extends Frontend
 			}
 
 			# convert from tl_additional_source to tl_theme_plus_file
-			if ($blnTableThemePlusFileExists && $this->Database->tableExists('tl_additional_source', false, true)) {
+			if ($blnTableThemePlusFileExists && $this->Database->tableExists('tl_additional_source', null, true)) {
 				try {
 					$this->Database->executeUncached("INSERT INTO tl_theme_plus_file (id,pid,sorting,tstamp,type,js_file,js_url,css_file,css_url,media,editor_integration,force_editor_integration)
 							SELECT id,pid,sorting,tstamp,type,js_file,js_url,css_file,css_url,media,editor_integration,force_editor_integration FROM tl_additional_source");
@@ -278,7 +283,7 @@ class ThemePlusRunonce extends Frontend
 			}
 
 			# drop tl_additional_source
-			if ($this->Database->tableExists('tl_additional_source', false, true)) {
+			if ($this->Database->tableExists('tl_additional_source', null, true)) {
 				try {
 					$this->Database->executeUncached("DROP TABLE tl_additional_source");
 				} catch (Exception $e) {
@@ -437,6 +442,176 @@ class ThemePlusRunonce extends Frontend
 			// disable theme_plus_exclude_frameworkcss to prevent overwrite of skipFramework next time
 			$this->Database
 				->query('UPDATE tl_layout SET theme_plus_exclude_frameworkcss=\'\' WHERE theme_plus_exclude_frameworkcss=\'1\'');
+		}
+	}
+
+
+	/**
+	 * Database upgrade to 3.0
+	 */
+	protected function upgrade3_0()
+	{
+		if ($this->Database->tableExists('tl_theme_plus_file', null, false)) {
+			// Upgrade from 3.0 alpha1
+			if (!$this->Database->fieldExists('sorting', 'tl_theme_plus_file', false) &&
+				$this->Database->fieldExists('theme_plus_stylesheets', 'tl_layout', false) &&
+				$this->Database->fieldExists('theme_plus_javascripts', 'tl_layout', false)) {
+
+				$this->Database->query('ALTER TABLE `tl_theme_plus_file` ADD `sorting` int(10) unsigned NOT NULL default \'0\'');
+
+				$arrCssSorting = array();
+				$arrJsSorting = array();
+
+				// collect all files, set index count to 0 for all
+				$objFile = $this->Database->query('SELECT * FROM tl_theme_plus_file', true);
+				while ($objFile->next()) {
+					if (preg_match('#^css_#', $objFile->type)) {
+						$arrCssSorting[$objFile->id] = 0;
+					}
+					else if (preg_match('#^js_#', $objFile->type)) {
+						$arrJsSorting[$objFile->id] = 0;
+					}
+				}
+
+				// collect the index count of each file
+				// (may not be the best way to find out the sorting of files)
+				$objLayout = $this->Database->query('SELECT * FROM tl_layout', false);
+				while ($objLayout->next()) {
+					$arrFiles = deserialize($objLayout->theme_plus_stylesheets, true);
+					foreach ($arrFiles as $index=>$id) {
+						if (isset($arrCssSorting[$id])) {
+							$arrCssSorting[$id] += $index + 1;
+						}
+					}
+					$arrFiles = deserialize($objLayout->theme_plus_javascripts, true);
+					foreach ($arrFiles as $index=>$id) {
+						if (isset($arrJsSorting[$id])) {
+							$arrJsSorting[$id] += $index + 1;
+						}
+					}
+				}
+
+				// put unused files to the end
+				$intMax = max($arrCssSorting);
+				foreach ($arrCssSorting as $id=>$count) {
+					if ($count == 0) {
+						$arrCssSorting[$id] = ++$intMax;
+					}
+				}
+				$intMax = max($arrJsSorting);
+				foreach ($arrJsSorting as $id=>$count) {
+					if ($count == 0) {
+						$arrJsSorting[$id] = ++$intMax;
+					}
+				}
+
+				// sort the files by index position count
+				asort($arrCssSorting);
+				asort($arrJsSorting);
+
+				// update sorting
+				$intSorting = 128;
+				foreach ($arrCssSorting as $id=>$count) {
+					$this->Database
+						->prepare('UPDATE tl_theme_plus_file SET sorting=? WHERE id=?')
+						->execute($intSorting, $id);
+					$intSorting *= 2;
+				}
+				$intSorting = 128;
+				foreach ($arrJsSorting as $id=>$count) {
+					$this->Database
+						->prepare('UPDATE tl_theme_plus_file SET sorting=? WHERE id=?')
+						->execute($intSorting, $id);
+					$intSorting *= 2;
+				}
+			}
+
+			if ($this->Database->fieldExists('theme_plus_files', 'tl_layout', false) &&
+				!$this->Database->fieldExists('theme_plus_stylesheets', 'tl_layout', false) &&
+				!$this->Database->fieldExists('theme_plus_javascripts', 'tl_layout', false)) {
+				// create new fields
+				$this->Database->query('ALTER TABLE `tl_layout` ADD `theme_plus_stylesheets` blob NULL');
+				$this->Database->query('ALTER TABLE `tl_layout` ADD `theme_plus_javascripts` blob NULL');
+				// set values
+				$objLayout = $this->Database->query('SELECT * FROM tl_layout', false);
+				while ($objLayout->next()) {
+					$arrFiles = deserialize($objLayout->theme_plus_files, true);
+
+					if (count($arrFiles)) {
+						$arrCSS = $this->Database
+							->query('SELECT * FROM tl_theme_plus_file WHERE id IN (' . implode(',', $arrFiles) . ') AND type LIKE \'css_%\'')
+							->fetchEach('id');
+						$arrJS = $this->Database
+							->query('SELECT * FROM tl_theme_plus_file WHERE id IN (' . implode(',', $arrFiles) . ') AND type LIKE \'js_%\'')
+							->fetchEach('id');
+
+						$this->Database
+							->prepare('UPDATE tl_layout SET theme_plus_stylesheets=?, theme_plus_javascripts=? WHERE id=?')
+							->execute(serialize($arrCSS), serialize($arrJS), $objLayout->id);
+					}
+				}
+
+				// drop obsolet field
+				$this->Database->query('ALTER TABLE `tl_layout` DROP `theme_plus_files`');
+			}
+
+			if ($this->Database->fieldExists('theme_plus_files', 'tl_page', false) &&
+				!$this->Database->fieldExists('theme_plus_stylesheets', 'tl_page', false) &&
+				!$this->Database->fieldExists('theme_plus_javascripts', 'tl_page', false)) {
+				// create new fields
+				$this->Database->query('ALTER TABLE `tl_page` ADD `theme_plus_stylesheets` blob NULL');
+				$this->Database->query('ALTER TABLE `tl_page` ADD `theme_plus_javascripts` blob NULL');
+				// set values
+				$objPage = $this->Database->query('SELECT * FROM tl_page', false);
+				while ($objPage->next()) {
+					$arrFiles = deserialize($objPage->theme_plus_files, true);
+
+					if (count($arrFiles)) {
+						$arrCSS = $this->Database
+							->query('SELECT * FROM tl_theme_plus_file WHERE id IN (' . implode(',', $arrFiles) . ') AND type LIKE \'css_%\'')
+							->fetchEach('id');
+						$arrJS = $this->Database
+							->query('SELECT * FROM tl_theme_plus_file WHERE id IN (' . implode(',', $arrFiles) . ') AND type LIKE \'js_%\'')
+							->fetchEach('id');
+
+						$this->Database
+							->prepare('UPDATE tl_page SET theme_plus_stylesheets=?, theme_plus_javascripts=? WHERE id=?')
+							->execute(serialize($arrCSS), serialize($arrJS), $objPage->id);
+					}
+				}
+
+				// drop obsolet field
+				$this->Database->query('ALTER TABLE `tl_page` DROP `theme_plus_files`');
+			}
+
+			if ($this->Database->fieldExists('theme_plus_files_noinherit', 'tl_page', false) &&
+				!$this->Database->fieldExists('theme_plus_stylesheets_noinherit', 'tl_page', false) &&
+				!$this->Database->fieldExists('theme_plus_javascripts_noinherit', 'tl_page', false)) {
+				// create new fields
+				$this->Database->query('ALTER TABLE `tl_page` ADD `theme_plus_stylesheets_noinherit` blob NULL');
+				$this->Database->query('ALTER TABLE `tl_page` ADD `theme_plus_javascripts_noinherit` blob NULL');
+				// set values
+				$objPage = $this->Database->query('SELECT * FROM tl_page', false);
+				while ($objPage->next()) {
+					$arrFiles = deserialize($objPage->theme_plus_files_noinherit, true);
+
+					if (count($arrFiles)) {
+						$arrCSS = $this->Database
+							->query('SELECT * FROM tl_theme_plus_file WHERE id IN (' . implode(',', $arrFiles) . ') AND type LIKE \'css_%\'')
+							->fetchEach('id');
+						$arrJS = $this->Database
+							->query('SELECT * FROM tl_theme_plus_file WHERE id IN (' . implode(',', $arrFiles) . ') AND type LIKE \'js_%\'')
+							->fetchEach('id');
+
+						$this->Database
+							->prepare('UPDATE tl_page SET theme_plus_stylesheets_noinherit=?, theme_plus_javascripts_noinherit=? WHERE id=?')
+							->execute(serialize($arrCSS), serialize($arrJS), $objPage->id);
+					}
+				}
+
+				// drop obsolet field
+				$this->Database->query('ALTER TABLE `tl_page` DROP `theme_plus_files_noinherit`');
+			}
 		}
 	}
 
